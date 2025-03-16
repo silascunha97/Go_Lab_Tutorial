c9197596e28830ebd572dc39ad61495c1341836d
Silas Augusto da Cunha <0997s.a.cunha@gmail.com>
2025-03-16 01:26:15 -0300

    criando rotas controller e model

--- CMD/main.go
+++ CMD/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin" // Importa o pacote gin, que é um framework web para Go
+	"localhost.com/GoLab/controller"
 )
 
 func main() {
+	// Função principal onde a execução do programa começa
 
 	server := gin.Default()
+	// Cria uma nova instância do servidor Gin com as configurações padrão
+
+	ProductController := controller.NewProductController()
 
 	server.GET("/ping", func(ctx *gin.Context) {
+		// Define uma rota GET para o caminho "/ping"
+		// Quando essa rota é acessada, a função anônima é executada
+
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
+		// Responde à requisição com um status HTTP 200 (OK) e um JSON contendo {"message": "pong"}
 	})
 
+	server.GET("/products", ProductController.GetProducts)
+	// Define uma rota GET para o caminho "/products"
+	// Quando essa rota é acessada, o método GetProducts do controlador de produtos é executado
+	// O método GetProducts retorna uma lista de produtos em formato JSON
+
 	server.Run(":8080")
+	// Inicia o servidor na porta 8080
 }
